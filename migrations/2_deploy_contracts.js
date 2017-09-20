bd7661eb9e9be734caf53f5f0bfeb7548b5979d2
jcxk <jcxkdev@gmail.com>
2017-09-20 11:03:42 +0200

    add useful commands

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,11 +1,13 @@
 var BettingonUITestDeploy = artifacts.require("./BettingonUITestDeploy.sol");
 
+//rinkeby oraclize "0x146500cfd35B22E4A392Fe0aDc06De1a1368Ed48",
+
 module.exports = function(deployer) {
 
     deployer.deploy(
         BettingonUITestDeploy,
         "0x6f485c8bf6fc43ea212e93bbf8ce046c7f1cb475",
-        {gas:470000000}
+         {gas:5000000}
     ).then(async () =>  {
         var d = await BettingonUITestDeploy.deployed();
         let pu = await d.pu();
